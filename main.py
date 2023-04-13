3c458e79c7df8b19b27a9966148870d936880ba2
vovod <nguyenhaid0212@gmail.com>
2023-04-13 15:23:19 +0700

    logs

--- main.py
+++ main.py
@@ -3,28 +3,7 @@ import os, sys
 import face_recognition_custom
 import cv2
 import numpy as np
-import datetime
-
-today = datetime.datetime.now()
-day = today.date()
-
-def write_logs(names):
-    with open('check.txt') as f:
-        lines = f.read().splitlines()
-    list_log = []
-    for i in range(len(lines)):
-        lines[i] = lines[i].split(" ")
-        if str(day) == lines[i][1]:
-            if lines[i][0] not in list_log:
-                list_log.append(lines[i][0])
-    for name in names:
-        full_logs = str(name) + " " + str(today)
-        # print(full_logs)
-        if name not in list_log:
-            f = open("check.txt", "a")
-            f.write(full_logs + "\n")
-            f.close()
-
+from write_logs import write_logs
 
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
@@ -56,7 +35,7 @@ class FaceRegconition:
             
             self.known_face_encodings.append(face_encoding)
             self.known_face_names.append(image)           
-        print(self.known_face_names) 
+        # print(self.known_face_names) 
     
     def run_recognition(self):
         video_capture = cv2.VideoCapture(0)
@@ -72,7 +51,7 @@ class FaceRegconition:
                 rgb_small_frame = small_frame[:, :, ::-1]
                 
                 self.face_locations = face_recognition_custom.face_locations(rgb_small_frame)
-                self.face_encodings = face_recognition_custom.face_encodings(rgb_small_frame, self.face_locations)     
+                self.face_encodings = face_recognition_custom.face_encodings(rgb_small_frame, self.face_locations, model="small")     
                 
                 self.face_names = []
                 short_names = []
@@ -98,15 +77,16 @@ class FaceRegconition:
                 right *= 4
                 bottom *= 4
                 left *= 4
-                
-                if name == "UNK":
+                if name[:3] == "UNK":
                     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
                     cv2.rectangle(frame, (left, bottom -35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                    cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
                 else:
                     cv2.rectangle(frame, (left, top), (right, bottom), (50,205,50), 2)
                     cv2.rectangle(frame, (left, bottom -35), (right, bottom), (50,205,50), cv2.FILLED)
-                    write_logs(short_names)
-                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+                    cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+                    write_logs(short_names, frame)
+                
                 
             cv2.imshow('Face Recognition', frame)
             
---
+++ write_logs.py
@@ -0,0 +1,34 @@
+import datetime
+import cv2
+import os
+
+def write_logs(names, frame):
+    today = datetime.datetime.now()
+    day = today.strftime("%Y-%m-%d")
+    with open('check.txt') as f:
+        lines = f.read().splitlines()
+    list_log = []
+    for i in range(len(lines)):
+        lines[i] = lines[i].split(" ")
+        if str(day) == lines[i][1]:
+            if lines[i][0] not in list_log:
+                list_log.append(lines[i][0])
+    for name in names:
+        full_logs = str(name) + " " + str(today)
+        # print(full_logs)
+        if name not in list_log:
+            path_log_images = "log_images"
+            cv2.imshow("catch", frame)
+            print("New log" + full_logs)
+            path_out = path_log_images + "/" + str(day)
+            if not os.path.exists(path_out):
+                os.makedirs(path_out)
+            file_name = name + ".jpg"
+            save_path = path_out + "/" + file_name
+            # print(save_path)
+            cv2.imwrite(save_path, frame)
+            f = open("check.txt", "a")
+            f.write(full_logs + "\n")
+            f.close()
+
+# print(day)
